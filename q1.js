f340445ecf31538148a3fec4cafd6be6950f267d
adityabhosle-1 <68443991+adityabhosle-1@users.noreply.github.com>
2021-05-10 14:47:20 +0530

    Add files via upload

---
+++ q1.js
@@ -0,0 +1,127 @@
+// Creating an array and passing the number, questions, options and answers
+
+let questions = [
+   {
+     numb: 1,
+     question: "The series Friends is set in which city?",
+     answer: "New York City",
+     options: [
+         "Los Angeles",
+         "New York City",
+         "Miami",
+         "Seattle"
+     ]
+
+   },
+   {
+     numb: 2,
+     question: "What is Monica skilled at?",
+     answer: "Cooking",
+     options: [
+         "Singing",
+         "American football",
+         "Cooking",
+         "Bricklaying"
+     ]
+
+   },
+   {
+     numb: 3,
+     question: "Monica briefly dates billionaire Pete Becker. Which country does he take her for their first date??",
+     answer: "Italy",
+     options: [
+         "France",
+         "England",
+         "Greece",
+         "Italy"
+     ]
+
+   },
+   {
+     numb: 4,
+     question: "What’s the name of the 1950s-themed diner where Monica worked as a waitress?",
+     answer: "Moondance Diner",
+     options: [
+         "Marilyn & Audrey",
+         "Moondance Diner",
+         "Twilight Galaxy",
+         "Marvin’s"
+     ]
+
+   },
+   {
+     numb: 5,
+     question: "What’s the name of Janice’s first husband??",
+     answer: "Gary Litman",
+     options: [
+         "Gary Litman",
+         "Sid Goralnik",
+         "Rob Bailystock",
+         "Nick Layster"
+     ]
+
+   },
+   {
+     numb: 6,
+     question: "What job does Ross have??",
+     answer: "Paleontologist",
+     options: [
+         "Paleontologist",
+         "Artist",
+         "Photographer",
+         "Insurance salesman"
+     ]
+
+   },
+   {
+     numb: 7,
+     question: "What’s the name of Joey’s penguin?",
+     answer: "Huggsy",
+     options: [
+         "Snowflake",
+         "Waddle",
+         "Huggsy",
+         "Bobber"
+     ]
+
+   },
+   {
+     numb: 8,
+     question: "What is Chandler’s middle name?",
+     answer: "Muriel",
+     options: [
+         "Jason",
+         "Kim",
+         "Zachary",
+         "Muriel"
+     ]
+
+   },
+   {
+     numb: 9,
+     question: "What is Janice most likely to say?",
+     answer: "Oh… my… God!",
+     options: [
+         "Talk to the hand!",
+         "Get me a coffee!",
+         "Oh… my… God!",
+         "No way!"
+     ]
+
+   },
+   {
+     numb: 10,
+     question: "Who sang the Friends theme?",
+     answer: "The Rembrandts",
+     options: [
+         "The Da Vinci Band",
+         "The Rembrandts",
+         "The Constables",
+         "The Banksys"
+     ]
+
+   }
+
+
+
+];
